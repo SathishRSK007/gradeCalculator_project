d50b9be954ec0cdc29773041112f7c6afc6c5b23
SathishRSK007 <sathishrsk1994@gmail.com>
2025-08-26 13:32:50 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,12 @@
+ // Takes user input (marks for five subjects)
+        var a =parseInt(prompt("Enter the marks of Tamil"));
+        var b =parseInt(prompt("Enter the marks of English"));
+        var c =parseInt(prompt("Enter the marks of Maths"));
+        var d =parseInt(prompt("Enter the marks of Science"));
+        var e =parseInt(prompt("Enter the marks of Socialscience"));
+        //Calculates the total marks
+        var total=a+b+c+d+e;
+        //Computes the average marks.
+        var average=total/5;
+
+          
